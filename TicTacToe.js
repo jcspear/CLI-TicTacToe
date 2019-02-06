f2424045674da6e88dd0d869f0ffbc98134b96d7
Joseph Spear <jcspear90@gmail.com>
2019-02-06 14:30:36 -0800

    Can successfully place pieces

--- TicTacToe.js
+++ TicTacToe.js
@@ -25,7 +25,12 @@ const placePiece = (player, position) => {
   })
 }
 
-playerMove('X');
+const playGame = (player1) => {
+  let player2 = player1 === 'X' ? 'O':'X';
+  return playerMove(player1).then(() => playGame(player2));
+}
+
+playGame('X');
 //use readline to accept input from users
 //have a promise chain
 //alternating players input coordinates for their move
